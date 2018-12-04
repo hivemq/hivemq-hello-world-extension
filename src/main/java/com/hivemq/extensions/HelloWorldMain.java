bfaed28072d4a8c5984743d8fd8b77920b821153
Florian Limpoeck <florian.limpoeck@gmail.com>
2018-12-04 09:39:01 +0100

    version 4.0.0

---
+++ src/main/java/com/hivemq/extensions/HelloWorldMain.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2018 dc-square GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hivemq.extensions;
+
+import com.hivemq.extension.sdk.api.ExtensionMain;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.events.EventRegistry;
+import com.hivemq.extension.sdk.api.parameter.*;
+import com.hivemq.extension.sdk.api.services.Services;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is the main class of the extension,
+ * which is instantiated either during the HiveMQ start up process (if extension is enabled)
+ * or when HiveMQ is already started by enabling the extension.
+ *
+ * @author Florian Limpöck
+ * @since 4.0.0
+ */
+public class HelloWorldMain implements ExtensionMain {
+
+    private static final @NotNull Logger log = LoggerFactory.getLogger(HelloWorldMain.class);
+
+    @Override
+    public void extensionStart(final @NotNull ExtensionStartInput extensionStartInput, final @NotNull ExtensionStartOutput extensionStartOutput) {
+
+        try {
+
+            addClientLifecycleEventListener();
+
+            final ExtensionInformation extensionInformation = extensionStartInput.getExtensionInformation();
+            log.info("Started " + extensionInformation.getName() + ":" + extensionInformation.getVersion());
+
+        } catch (Exception e) {
+            log.error("Exception thrown at extension start: ", e);
+        }
+
+    }
+
+    @Override
+    public void extensionStop(final @NotNull ExtensionStopInput extensionStopInput, final @NotNull ExtensionStopOutput extensionStopOutput) {
+
+        final ExtensionInformation extensionInformation = extensionStopInput.getExtensionInformation();
+        log.info("Stopped " + extensionInformation.getName() + ":" + extensionInformation.getVersion());
+
+    }
+
+    private void addClientLifecycleEventListener() {
+
+        final EventRegistry eventRegistry = Services.eventRegistry();
+
+        final HelloWorldListener helloWorldListener = new HelloWorldListener();
+
+        eventRegistry.setClientLifecycleEventListener(input -> helloWorldListener);
+
+    }
+
+}
