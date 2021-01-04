a6cfcf37f44d1a4a3e5fa88cd27bf30865142247
Anja Helmbrecht-Schaar <anja.helmbrecht-schaar@dc-square.de>
2021-01-04 11:47:30 +0100

    Added Gradle build setup for Extension, removed unnecessary files (from Maven setup), adapt Readme

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+
+plugins {
+    id("com.hivemq.extension")
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+/* ******************** metadata ******************** */
+
+group = "com.hivemq.extensions"
+description = "HiveMQ Hello World Extension"
+version = "4.4.4"
+
+
+hivemqExtension {
+    name = "HiveMQ Hello World Extension"
+    author = "HiveMQ"
+    priority = 1000
+    startPriority = 1000
+    mainClass = "$group.HelloWorldMain"
+    sdkVersion = "4.4.3"
+}
+
+dependencies {
+    hivemqProvided ("ch.qos.logback:logback-classic:1.2.3")
+}
+
+dependencies {
+    testImplementation ("com.hivemq:hivemq-mqtt-client:1.2.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
+    testImplementation("com.hivemq:hivemq-testcontainer-junit5:1.1.1")
+}
+
+val unzipHivemq by tasks.registering(Sync::class) {
+    from(zipTree(rootDir.resolve("/your/path/to/hivemq-<VERSION>.zip")))
+    into({ temporaryDir })
+}
+
+tasks.prepareHivemqHome {
+    hivemqFolder.set(unzipHivemq.map { it.destinationDir.resolve("hivemq-<VERSION>") } as Any)
+}
+
+tasks.runHivemqWithExtension {
+    debugOptions {
+        enabled.set(false)
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "HelloWorldExtension"
+rootProject.buildFileName = "build.gradle.kts"
+
+pluginManagement {
+    plugins {
+        id("com.hivemq.extension") version "1.0.0"
+    }
+}
