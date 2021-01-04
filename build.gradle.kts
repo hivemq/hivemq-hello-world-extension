079e40bd6ee755b65eba42cda25cb3d5615821c2
Anja Helmbrecht-Schaar <anja.helmbrecht-schaar@dc-square.de>
2021-01-04 17:12:32 +0100

    Added plugins and tasks for readme and license preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 
 plugins {
     id("com.hivemq.extension")
+    id("com.github.hierynomus.license")
+    id("com.github.sgtsilvio.gradle.utf8")
+    id("org.asciidoctor.jvm.convert")
 }
 
 group = "com.hivemq.extensions"
 description = "HiveMQ Hello World Extension"
 version = "4.4.4"
-
 
 hivemqExtension {
     name = "HiveMQ Hello World Extension"
@@ -19,11 +21,32 @@ hivemqExtension {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.1")
     testImplementation("com.hivemq:hivemq-mqtt-client:1.2.1")
     testImplementation("com.hivemq:hivemq-testcontainer-junit5:1.1.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.1")
 }
 
+val prepareAsciidoc by tasks.registering(Sync::class) {
+    from("README.adoc").into({ temporaryDir })
+}
+
+tasks.asciidoctor {
+    dependsOn(prepareAsciidoc)
+    sourceDir(prepareAsciidoc.map { it.destinationDir })
+}
+
+tasks.hivemqExtensionResources {
+    from("LICENSE")
+    from("README.adoc") { rename { "README.txt" } }
+    from(tasks.asciidoctor)
+}
+
+license {
+    header = rootDir.resolve("HEADER")
+    mapping("java", "SLASHSTAR_STYLE")
+}
+
+//preparation and tasks to run & debug Hello World Extension locally
 val unzipHivemq by tasks.registering(Sync::class) {
     from(zipTree(rootDir.resolve("/your/path/to/hivemq-<VERSION>.zip")))
     into({ temporaryDir })
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,8 @@ rootProject.name = "hivemq-hello-world-extension"
 pluginManagement {
     plugins {
         id("com.hivemq.extension") version "1.0.0"
+        id("com.github.hierynomus.license") version "0.15.0"
+        id("com.github.sgtsilvio.gradle.utf8") version "0.1.0"
+        id("org.asciidoctor.jvm.convert") version "3.2.0"
     }
 }
