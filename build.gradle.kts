1bfa4210c182cdf1cc413021051e39a57a63c6c9
Anja Helmbrecht-Schaar <anja.helmbrecht-schaar@hivemq.com>
2021-01-07 13:34:30 +0100

    Added Gradle build setup for Extension (#6)
    * Added Gradle build setup for Extension, removed unnecessary files (from Maven setup)
    * Update README.adoc
    * Uniform .gitignore
    * Updated CONTRIBUTING.md

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.hivemq.extension")
+    id("com.github.hierynomus.license")
+    id("com.github.sgtsilvio.gradle.utf8")
+    id("org.asciidoctor.jvm.convert")
+}
+
+group = "com.hivemq.extensions"
+description = "HiveMQ 4 Hello World Extension - a simple reference for all extension developers"
+
+hivemqExtension {
+    name = "Hello World Extension"
+    author = "HiveMQ"
+    priority = 1000
+    startPriority = 1000
+    mainClass = "$group.HelloWorldMain"
+    sdkVersion = "$version"
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit-jupiter.version")}")
+    testImplementation("com.hivemq:hivemq-mqtt-client:${property("hivemq-mqtt-client.version")}")
+    testImplementation("com.hivemq:hivemq-testcontainer-junit5:${property("hivemq-testcontainer.version")}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${property("junit-jupiter.version")}")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+val prepareAsciidoc by tasks.registering(Sync::class) {
+    from("README.adoc").into({ temporaryDir })
+}
+
+tasks.asciidoctor {
+    dependsOn(prepareAsciidoc)
+    sourceDir(prepareAsciidoc.map { it.destinationDir })
+}
+
+tasks.hivemqExtensionResources {
+    from("LICENSE")
+    from("README.adoc") { rename { "README.txt" } }
+    from(tasks.asciidoctor)
+}
+
+license {
+    header = rootDir.resolve("HEADER")
+    mapping("java", "SLASHSTAR_STYLE")
+}
+
+//preparation and tasks to run & debug Hello World Extension locally
+val unzipHivemq by tasks.registering(Sync::class) {
+    from(zipTree(rootDir.resolve("/your/path/to/hivemq-<VERSION>.zip")))
+    into({ temporaryDir })
+}
+
+tasks.prepareHivemqHome {
+    hivemqFolder.set(unzipHivemq.map { it.destinationDir.resolve("hivemq-<VERSION>") } as Any)
+}
+
+tasks.runHivemqWithExtension {
+    debugOptions {
+        enabled.set(false)
+    }
+}
--- src/test/java/com/hivemq/extensions/HelloWorldInterceptorIT.java
+++ src/test/java/com/hivemq/extensions/HelloWorldInterceptorIT.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2018-present HiveMQ GmbH
  *
@@ -6,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hivemq.extensions;
 
 import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
@@ -22,11 +20,12 @@ import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
-import com.hivemq.testcontainer.core.MavenHiveMQExtensionSupplier;
+import com.hivemq.testcontainer.core.GradleHiveMQExtensionSupplier;
 import com.hivemq.testcontainer.junit5.HiveMQTestContainerExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
+
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
@@ -45,7 +44,7 @@ class HelloWorldInterceptorIT {
     @RegisterExtension
     public final @NotNull HiveMQTestContainerExtension extension =
             new HiveMQTestContainerExtension()
-                    .withExtension(MavenHiveMQExtensionSupplier.direct().get());
+                    .withExtension(GradleHiveMQExtensionSupplier.direct().get());
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
