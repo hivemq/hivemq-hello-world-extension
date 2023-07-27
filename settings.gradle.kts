784d9f957fd1034fb39efd59d2f94ea69852192e
Silvio Giebl <silvio.giebl@hivemq.com>
2023-07-27 17:55:54 +0200

    Cleanup build scripts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "hivemq-hello-world-extension"
 pluginManagement {
     plugins {
         id("com.hivemq.extension") version "${extra["plugin.hivemq-extension.version"]}"
+        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
         id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
     }
 }
