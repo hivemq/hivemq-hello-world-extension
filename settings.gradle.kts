ef972512b32cfa18c5d83ab177ca26d1b43385ed
Silvio Giebl <silvio.giebl@hivemq.com>
2023-07-30 17:37:55 +0200

    Add libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
 rootProject.name = "hivemq-hello-world-extension"
-
-pluginManagement {
-    plugins {
-        id("com.hivemq.extension") version "${extra["plugin.hivemq-extension.version"]}"
-        id("io.github.sgtsilvio.gradle.defaults") version "${extra["plugin.defaults.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-    }
-}
