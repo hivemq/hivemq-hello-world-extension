2bae00db6a39c72845de8852135d9b1ae2e294fb
David Sondermann <david.sondermann@hivemq.com>
2026-08-04 17:50:47 +0200

    Add mockito agent configuration for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,27 @@ hivemqExtension {
     }
 }
 
+val mockitoAgent = configurations.create("mockitoAgent") {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+dependencies {
+    mockitoAgent(libs.mockito) { isTransitive = false }
+}
+class MockitoAgentArgumentProvider(@get:Classpath val agentJar: FileCollection) : CommandLineArgumentProvider {
+    override fun asArguments(): Iterable<String> = listOf("-javaagent:${agentJar.singleFile}")
+}
+
 @Suppress("UnstableApiUsage")
 testing {
     suites {
         withType<JvmTestSuite> {
             useJUnitJupiter(libs.versions.junit.jupiter)
+            targets.configureEach {
+                testTask {
+                    jvmArgumentProviders.add(MockitoAgentArgumentProvider(mockitoAgent))
+                }
+            }
         }
         "test"(JvmTestSuite::class) {
             dependencies {
